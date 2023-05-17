b34b2840e099345910478d0779cc4839461b4485
Tpf2906 <tiagopassosferreira@gmail.com>
2023-05-17 11:09:36 +0100

    Added cameras and changed background colour

--- Template - Trabalhos B e C/js/main-script.js
+++ Template - Trabalhos B e C/js/main-script.js
@@ -1,11 +1,13 @@
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
-var camera, scene, renderer;
+var cameras=[],cameraX,cameraY,cameraZ,perspectiveCamera,orthographicCamera, scene, renderer;
 
-var geometry, material, mesh;
+var geometry, material=[], mesh;
 
 var keys = {};
+
+var camera_index;
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -16,6 +18,7 @@ function createScene() {
   scene = new THREE.Scene();
 
   scene.add(new THREE.AxisHelper(10));
+  scene.background = new THREE.Color(0xa9f5ee);
 
   CreateTrailer(0, 1, 0);
 }
@@ -25,17 +28,50 @@ function createScene() {
 //////////////////////
 function createCamera() {
   "use strict";
-  camera = new THREE.PerspectiveCamera(
-    70,
-    window.innerWidth / window.innerHeight,
+  perspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  perspectiveCamera.position.set(20, 20, 20);
+  perspectiveCamera.lookAt(scene.position);
+
+  // Create an orthographic camera
+  orthographicCamera = new THREE.OrthographicCamera(
+    window.innerWidth / -2,
+    window.innerWidth / 2,
+    window.innerHeight / 2,
+    window.innerHeight / -2,
     1,
-    1000
+    40
   );
-  camera.position.x = 20;
-  camera.position.y = 20;
-  camera.position.z = 20;
-  camera.lookAt(scene.position);
-}
+  orthographicCamera.position.set(0, 0, 5);
+  orthographicCamera.lookAt(scene.position);
+
+  // Create a camera aligned with the X axis
+  cameraX = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  cameraX.position.set(15, 0, 0);
+  cameraX.lookAt(new THREE.Vector3(0, 0, 0));
+
+  // Create a camera aligned with the Y axis
+  cameraY = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  cameraY.position.set(0, 15, 0);
+  cameraY.lookAt(new THREE.Vector3(0, 0, 0));
+
+  // Create a camera aligned with the Z axis
+  cameraZ = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  cameraZ.position.set(0, 0, 15);
+  cameraZ.lookAt(new THREE.Vector3(0, 0, 0));
+
+  cameras.push(perspectiveCamera);
+  cameras.push(orthographicCamera);
+  cameras.push(cameraX);
+  cameras.push(cameraY);
+  cameras.push(cameraZ);
+
+  scene.add(perspectiveCamera);
+  scene.add(orthographicCamera);
+  scene.add(cameraX);
+  scene.add(cameraY);
+  scene.add(cameraZ);
+
+  }
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -45,7 +81,7 @@ function createCamera() {
 /* CREATE OBJECT3D(S) */
 ////////////////////////
 function addBox(obj, x, y, z) {
-  geometry = new THREE.CubeGeometry(2, 2, 6);
+  geometry = new THREE.BoxGeometry(2, 2, 6);
   mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(x, y, z);
   obj.add(mesh);
@@ -102,7 +138,7 @@ function update() {
 /////////////
 /* DISPLAY */
 /////////////
-function render() {
+function render(camera) {
   "use strict";
   renderer.render(scene, camera);
 }
@@ -120,8 +156,9 @@ function init() {
 
   createScene();
   createCamera();
+  camera_index = 0;
 
-  render();
+  render(cameras[camera_index]);
 
   window.addEventListener("keydown", handleKeyDown);
   window.addEventListener("keyup", handleKeyUp);
@@ -133,7 +170,7 @@ function init() {
 /////////////////////
 function animate() {
   "use strict";
-  render();
+  render(cameras[camera_index]);
 
   requestAnimationFrame(animate);
 }
@@ -147,8 +184,10 @@ function onResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   if (window.innerHeight > 0 && window.innerWidth > 0) {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    for (var i = 0; i < cameras.length; i++){
+      cameras[i].aspect = window.innerWidth / window.innerHeight;
+      cameras[i].updateProjectionMatrix();
+    }
   }
 }
 
@@ -229,37 +268,23 @@ function onKeyDown() {
 
     //-------camera-------
     case keys[49]: //Digit1
-      camera.position.set(0, 20, 0);
-      camera.rotation.set(-Math.PI / 2, 0, 0);
+      camera_index = 0;
       break;
 
     case keys[50]: //Digit2
-      camera.position.set(0, 3, -10);
-      camera.rotation.set(0, -Math.PI, 0);
+      camera_index = 1;
       break;
 
     case keys[51]: //Digit3
-      scene.traverse(function (node) {
-        if (node instanceof THREE.Mesh) {
-          node.position.z += 0.1;
-        }
-      });
+      camera_index = 2;
       break;
 
     case keys[52]: //Digit4
-      scene.traverse(function (node) {
-        if (node instanceof THREE.Mesh) {
-          node.position.z += 0.1;
-        }
-      });
+      camera_index = 3;
       break;
 
     case keys[53]: //Digit5
-      scene.traverse(function (node) {
-        if (node instanceof THREE.Mesh) {
-          node.position.z += 0.1;
-        }
-      });
+      camera_index = 4;
       break;
   }
 }
